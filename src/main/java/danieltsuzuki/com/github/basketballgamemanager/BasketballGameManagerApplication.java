1058a721162f1d485f99459ed4de6211708b1d8b
daniel-tsuzuki-fairy <daniel.tsuzuki@soufairycode.com>
2025-04-11 04:31:22 -0300

    Init

---
+++ src/main/java/danieltsuzuki/com/github/basketballgamemanager/BasketballGameManagerApplication.java
@@ -0,0 +1,13 @@
+package danieltsuzuki.com.github.basketballgamemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketballGameManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasketballGameManagerApplication.class, args);
+    }
+
+}
